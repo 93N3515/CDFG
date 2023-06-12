f86849ca12b40e2ebe7e9bf5fb798b3f7a052877
93N3515 <70991773+93N3515@users.noreply.github.com>
2023-06-12 18:48:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileGrabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Получаем путь из аргументов командной строки
+            string path = args[0];
+
+            // Получаем список всех каталогов в пути
+            string[] directories = Directory.GetDirectories(path);
+
+            foreach (string directory in directories)
+            {
+                // Соединяем имя каталога с "FileGrabber"
+                string targetDirectory = Path.Combine(directory, "FileGrabber");
+
+                // Проверяем, существует ли такой каталог
+                if (Directory.Exists(targetDirectory))
+                {
+                    // Удаляем каталог со всеми файлами в нем
+                    Directory.Delete(targetDirectory, true);
+                    Console.WriteLine("Folder " + directory + " Deleted.");
+                }
+            }
+        }
+    }
+}
